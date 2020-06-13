fe416d90ec46a742585473831ff3c57621b7d714
root <root@DESKTOP-N51U7HO.localdomain>
2020-06-13 15:37:43 +0200

    2 yml

--- lets-encrypt-nc.sh
+++ lets-encrypt-nc.sh
@@ -1,1 +1,7 @@
-sudo nextcloud.enable-https lets-encrypt <<< $'yes\nsolenbellouati@gmail.com\nnextcloud.solenb.fr'
+- hosts: all
+  tasks:
+    - name: Reset the let-encrypt certificates
+      become: yes
+      become_user: root
+      raw: nextcloud.enable-https lets-encrypt <<< $'yes\nsolenbellouati@gmail.com\nnextcloud.solenb.fr' 
+ 
